dd46827e413ca89a7c8815fa8221fc249024ec1d
KitsuX <zaidankhoir4@gmail.com>
2026-04-08 10:10:21 +0700

    Update and Sync

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         targetSdk = 36 // Target the latest SDK (36)
         versionCode = 1
         // Vesion code di ubah kalau rilis di playstore setiap update
-        versionName = "2.2.5.26"
+        versionName = "3.0.5.26"
         // Major = 1.0.0 > 2.0.0 perubahan total
         // Minor = 1.0.0 > 1.1.0 penambahan fitur
         // Patch = 1.0.0 > 1.0.1 perbaikan kecil
